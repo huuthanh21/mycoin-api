49e5705c2c6344f2ec3e109c7718d15f64cac9a5
20120582 <newacc2312@gmail.com>
2024-07-08 04:01:07 +0700

    feat: Add CryptoBlockchain class for managing the blockchain

---
+++ lib/CryptoBlockchain.ts
@@ -0,0 +1,28 @@
+import { CryptoBlock } from "./CryptoBlock";
+
+class CryptoBlockchain {
+	blockchain: CryptoBlock[];
+
+	constructor() {
+		this.blockchain = [this.startGenesisBlock()];
+	}
+
+	startGenesisBlock(): CryptoBlock {
+		return new CryptoBlock({
+			index: 0,
+			timestamp: "01/01/2021",
+			data: "Initial Block in the Chain",
+			precedingHash: " ",
+		});
+	}
+
+	obtainLatestBlock(): CryptoBlock {
+		return this.blockchain[this.blockchain.length - 1];
+	}
+
+	addNewBlock(newBlock: CryptoBlock) {
+		newBlock.precedingHash = this.obtainLatestBlock().hash;
+		newBlock.hash = newBlock.computeHash();
+		this.blockchain.push(newBlock);
+	}
+}
